410a1fe69f37cbf8f5834757e309b6256b94d572
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-03-04 16:51:17 +0800

    export useMetaData

--- src/web/index.ts
+++ src/web/index.ts
@@ -2,3 +2,4 @@ export * from './lib/Logger';
 export * from './lib/react/ErrorBoundary';
 export * from './lib/react/Portal';
 export * from './lib/react/hooks/useAsyncError';
+export * from './lib/react/useMetaData';
