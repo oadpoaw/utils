28d4da0e9169c7f416b88e7c62e59b25b64f9d2d
oadpoaw <46276781+oadpoaw@users.noreply.github.com>
2022-04-01 13:07:36 +0800

    may this work

---
+++ src/web/index.ts
@@ -0,0 +1,5 @@
+export * from './Logger.js';
+export * from './react/ErrorBoundary.js';
+export * from './react/Portal.js';
+export * from './react/useMetaData.js';
+export * from './react/hooks/useAsyncError.js';
